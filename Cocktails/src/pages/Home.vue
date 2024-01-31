832100df75714a085a134fce46c32ce9c6034986
IGOR EVSSEV <andrey.semin.evseev@gmail.com>
2024-01-31 18:33:37 +0300

    Стилизация Home (30.01.24)

--- Cocktails/src/pages/Home.vue
+++ Cocktails/src/pages/Home.vue
@@ -1,9 +1,15 @@
 <script setup>
+import { ref } from "vue";
 import AppLauout from "../components/AppLayout.vue";
 import { useRootStore } from "../stores/root";
+import { storeToRefs } from "pinia";
 
+// Работа с Phinia
 const rootStore = useRootStore();
 rootStore.getIngredients();
+
+const { ingredients } = storeToRefs(rootStore);
+const ingredient = ref(null);
 </script>
 
 <template>
@@ -12,6 +18,29 @@ rootStore.getIngredients();
       <div class="info">
         <div class="title">Choose your drink</div>
         <div class="line"></div>
+        <div class="select-wrapper">
+          <el-select
+            v-model="ingredient"
+            placeholder="Choose main ingredient"
+            size="large"
+            style="width: 240px"
+            class="select"
+          >
+            <el-option
+              v-for="item in ingredients"
+              :key="item.strIngredient1"
+              :label="item.strIngredient1"
+              :value="item.strIngredient1"
+            />
+          </el-select>
+
+          <div class="text">
+            Try our delicious cocktail recipes for every occasion. Find
+            delicious cocktail recipes by ingredient through our cocktail
+            generator.
+          </div>
+          <img src="../assets/img/glasses.png" alt="Cocktails" class="img" />
+        </div>
       </div>
     </div>
   </AppLauout>
@@ -28,4 +57,22 @@ rootStore.getIngredients();
 .info
   padding: 80px 0
   text-align: center
+.select-wrapper
+  padding-top: 50px
+
+.select
+  width: 220px
+
+.text
+  padding-top: 50px
+  max-width: 516px
+  color: $textMuted
+  font-size: 16px
+  font-weight: 400
+  line-height: 36px
+  letter-spacing: 1.6px
+
+.img
+  width: 345px
+  padding-top: 60px
 </style>
